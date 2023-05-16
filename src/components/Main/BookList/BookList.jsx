12a33d6c65c284b3b28e4f04be183314a61e720c
Jeremy Merlin <jeremymerlin@hotmail.fr>
2023-05-16 17:16:24 +0100

    lab 2 done

--- src/components/Main/BookList/BookList.jsx
+++ src/components/Main/BookList/BookList.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 
-export default function BookList({ books }) {
+export default function BookList({ books, deleteBook }) {
   let bookList = books.map((book) => {
     return (
       <article key={book._id} className="book-item">
         <p>Title: {book.title}</p>
         <p>Description: {book.description}</p>
         <p>Status: {book.status}</p>
+        <button onClick={() => deleteBook(book._id)}>Delete</button>
       </article>
     );
   });
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,12 +1,21 @@
 import axios from "axios";
-import React from "react";
+import React, { useEffect } from "react";
 import "./Main.css";
 import BookList from "./BookList/BookList";
 
 import { useState } from "react";
 
 export default function Main() {
-  const [books, setBooks] = useState("");
+  const [books, setBooks] = useState([]);
+  const [form, setForm] = useState({
+    title: "",
+    description: "",
+    status: "",
+  });
+
+  useEffect(() => {
+    getBooks();
+  }, []);
 
   async function getBooks() {
     try {
@@ -14,15 +23,46 @@ export default function Main() {
       const res = await axios.get(API);
 
       if (res.data) {
-        let books = <BookList books={res.data} />;
+        let books = <BookList books={res.data} deleteBook={deleteBook} />;
         setBooks(books);
       }
     } catch (error) {}
   }
 
+  async function deleteBook(id) {
+    try {
+      const API = `http://localhost:8080/books/${id}`;
+      await axios.delete(API);
+      getBooks();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async function postBook(event) {
+    event.preventDefault();
+    const API = `http://localhost:8080/books`;
+    await axios.post(API, form);
+    getBooks();
+    setForm({
+      title: "",
+      description: "",
+      status: "",
+    });
+  }
+
+  function handleChange(event) {
+    setForm({ ...form, [event.target.name]: event.target.value });
+  }
+
   return (
     <main>
-      <button onClick={() => getBooks()}>Load books</button>
+      <h1>Books</h1>
+      <form onSubmit={postBook}>
+        <input type="text" name="title" placeholder="title" onChange={handleChange} value={form.title} />
+        <input type="text" name="description" placeholder="description" onChange={handleChange} value={form.description} />
+        <input type="text" name="status" placeholder="status" onChange={handleChange} value={form.status} />
+        <input type="submit" />
+      </form>
       {books}
     </main>
   );
